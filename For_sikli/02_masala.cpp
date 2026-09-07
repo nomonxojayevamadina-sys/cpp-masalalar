2ef2ec31cb459444ce668eb89f24f1d9e0f0811f
madina no'monxo'jayeva <nomonxojayevamadina@gmail.com>
2026-09-07 13:11:55 +0500

    Create 02_masala.cpp

---
+++ For_sikli/02_masala.cpp
@@ -0,0 +1,16 @@
+// k sonini n marta takrorlang
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int k, n;
+  cout<<" kiritilgan k soni n marta chiqariladi!"<<endl;
+  cout<<"k="<<endl; cin>>k;
+  cout<<"n="<<endl; cin>>n;
+  for ( int i = 1 ; i<=n ; i++) {
+  cout<<k<<" ";
+  }
+  
+  return 0;
+}
